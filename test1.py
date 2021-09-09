836ec7fccddeaca62eae8e757531f40ea2ad4694
DanilChechkov <danilchechkov@icloud.com>
2021-09-09 19:34:40 +0300

    Теперь хэширование происходит по файлу, а не по его имени

--- test1.py
+++ test1.py
@@ -1,23 +1,27 @@
 import os
 import hashlib
-from flask import Flask, request, send_from_directory
+import shutil
+from flask import Flask, request, send_from_directory, render_template
 workDir = os.path.dirname((os.path.abspath(__file__)))      #Корневая дирректория
 app = Flask(__name__)
 
+def hashname(file):                                     #Функция хэширования SHA256
+    return hashlib.sha256(file.read()).hexdigest()
 
-def hashname(filename):                                     #Функция хэширования SHA256
-    return hashlib.sha256(filename.encode()).hexdigest()
 
 @app.route('/',methods=['POST'])                            #Получаем запрос на загрузку файла с главной
 def upload():
     if request.method == 'POST':
         file = request.files['file']
-        fname = hashname(file.filename)                     #Хэшируем имя файла и создаем дирректорию при необходимости
-        if not os.path.exists(os.path.join(workDir,'store')):
-            os.mkdir(os.path.join(workDir,'store'))                 
-        if not os.path.exists(os.path.join(workDir,'store',fname[:2])):
-            os.mkdir(os.path.join(workDir,'store',fname[:2]))
-        file.save(os.path.join(workDir,'store',fname[:2],fname))    #Наконец сохраняем файл на сервер
+        file.save(os.path.join(workDir,'tmp'))
+        fname = file.filename
+        with open(os.path.join(workDir,'tmp'),'rb') as f:
+            fname = hashname(f)                         #Хэшируем файл и создаем дирректорию при необходимости
+            if not os.path.exists(os.path.join(workDir,'store')):
+                os.mkdir(os.path.join(workDir,'store'))                 
+            if not os.path.exists(os.path.join(workDir,'store',fname[:2])):
+                os.mkdir(os.path.join(workDir,'store',fname[:2]))
+        shutil.move(os.path.join(workDir,'tmp'),os.path.join(workDir,'store',fname[:2],fname))    #Наконец сохраняем файл на сервер
         return fname                                        #Возвращаем пользователю хэшированное имя файла
     return 'That should not happen...'
 
