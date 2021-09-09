217b02a818183ae9ee287eef059768dc2157fcc9
DanilChechkov <danilchechkov@icloud.com>
2021-09-09 15:15:32 +0300

    Store adding

--- test1.py
+++ test1.py
@@ -5,31 +5,34 @@ workDir = os.path.dirname((os.path.abspath(__file__)))      #Корневая д
 app = Flask(__name__)
 
 
-def hashname(filename):                                     #Функция хэширования SHA256
+def hashname(filename):                                 
+    print(hashlib.sha256(filename.encode()).hexdigest())    #Функция хэширования SHA256
     return hashlib.sha256(filename.encode()).hexdigest()
 
 @app.route('/',methods=['POST'])                            #Получаем запрос на загрузку файла с главной
 def upload():
     if request.method == 'POST':
         file = request.files['file']
-        fname = hashname(file.filename)                     #Хэшируем имя файла и создаем дирректорию при необходимости
-        if not os.path.exists(os.path.join(workDir,fname[:2])):
-            os.mkdir(os.path.join(workDir,fname[:2]))
-        file.save(os.path.join(workDir,fname[:2],fname))    #Наконец сохраняем файл на сервер
+        fname = hashname(file.filename)    
+        if not os.path.exists(os.path.join(workDir,'store')):
+            os.mkdir(os.path.join(workDir,'store'))                 #Хэшируем имя файла и создаем дирректорию при необходимости
+        if not os.path.exists(os.path.join(workDir,'store',fname[:2])):
+            os.mkdir(os.path.join(workDir,'store',fname[:2]))
+        file.save(os.path.join(workDir,'store',fname[:2],fname))    #Наконец сохраняем файл на сервер
         return fname                                        #Возвращаем пользователю хэшированное имя файла
     return 'That should not happen...'
 
 @app.get('/<name>')                                         #Запрос на загрузку файла
 def dowloand(name):
     try:
-        return send_from_directory(os.path.join(workDir,name[:2]),name,as_attachment=True)
+        return send_from_directory(os.path.join(workDir,'store',name[:2]),name,as_attachment=True)
     except:
         return "NO SUCH FILE"                               #Если файла нет, то возращаем сообщения что такого файла нет
 
 @app.delete('/<name>')                                      #Удаление файла с сервера
 def delete(name):
     try:
-        os.remove(os.path.join(workDir,name[:2],name))
+        os.remove(os.path.join(workDir,'store',name[:2],name))
         return 'FILE REMOVED'
     except:
         return 'NO SUCH FILE'
