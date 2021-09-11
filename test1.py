304bfe00cad12814d0feb8e10e366007736fb6b0
DanilChechkov <danilchechkov@icloud.com>
2021-09-11 17:17:37 +0300

    Graphic interface add

--- test1.py
+++ test1.py
@@ -1,32 +1,49 @@
 import os
 import hashlib
+import re
 import shutil
 from flask import Flask, request, send_from_directory, render_template
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 workDir = os.path.dirname((os.path.abspath(__file__)))      #Корневая дирректория
-app = Flask(__name__)
+app = Flask(__name__,template_folder='template')
 
 def hashname(file):                                     #Функция хэширования SHA256
     return hashlib.sha256(file.read()).hexdigest()
 
 
-@app.route('/',methods=['POST'])                            #Получаем запрос на загрузку файла с главной
+@app.route('/',methods=['POST','GET'])                            #Получаем запрос на загрузку файла с главной
 def upload():
     if request.method == 'POST':
-        file = request.files['file']
-        file.save(os.path.join(workDir,'tmp'))
-        fname = file.filename
-        with open(os.path.join(workDir,'tmp'),'rb') as f:
-            fname = hashname(f)                         #Хэшируем файл и создаем дирректорию при необходимости
-            if not os.path.exists(os.path.join(workDir,'store')):
-                os.mkdir(os.path.join(workDir,'store'))                 
-            if not os.path.exists(os.path.join(workDir,'store',fname[:2])):
-                os.mkdir(os.path.join(workDir,'store',fname[:2]))
-        shutil.move(os.path.join(workDir,'tmp'),os.path.join(workDir,'store',fname[:2],fname))    #Наконец сохраняем файл на сервер
-        return fname                                        #Возвращаем пользователю хэшированное имя файла
-    return 'That should not happen...'
+        print(request.values.to_dict())
+        if request.files:
+            file = request.files['file']
+            file.save(os.path.join(workDir,'tmp'))
+            fname = file.filename
+            with open(os.path.join(workDir,'tmp'),'rb') as f:
+                fname = hashname(f)                         #Хэшируем файл и создаем дирректорию при необходимости
+                if not os.path.exists(os.path.join(workDir,'store')):
+                    os.mkdir(os.path.join(workDir,'store'))                 
+                if not os.path.exists(os.path.join(workDir,'store',fname[:2])):
+                    os.mkdir(os.path.join(workDir,'store',fname[:2]))
+            shutil.move(os.path.join(workDir,'tmp'),os.path.join(workDir,'store',fname[:2],fname))    #Наконец сохраняем файл на сервер
+            return fname                                        #Возвращаем пользователю хэшированное имя файла
+        elif 'name' in request.values:
+            print(request.values)
+            return redirect(url_for('download', name=request.values['name']))
+        elif 'delete' in request.values:
+            try:
+                os.remove(os.path.join(workDir,'store',request.values['delete'][:2],request.values['delete']))
+                return 'FILE REMOVED'
+            except:
+                return 'NO SUCH FILE'
+        else:
+            return render_template('index.html')
+    else:
+        return render_template('index.html')
 
 @app.get('/<name>')                                         #Запрос на загрузку файла
-def dowloand(name):
+def download(name):
     try:
         return send_from_directory(os.path.join(workDir,'store',name[:2]),name,as_attachment=True)
     except:
